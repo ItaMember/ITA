8e76ddef46fd0652bd4389381db187f32ea3b6be
Benedict <benedict@bridgetech.co.id>
2018-04-09 12:21:38 +0700

    add booking

---
+++ models/Booking.js
@@ -0,0 +1,21 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * User Model
+ * ==========
+ */
+var Booking = new keystone.List('Booking');
+
+Booking.add({
+	user: { type: Types.Relationship, ref:'User'},
+    event: { type: Types.Relationship, ref:'Event' },
+    status: { type: Types.Select, options: 'draft, published, archived', default: 'draft'}
+});
+
+
+/**
+ * Registration
+ */
+Booking.defaultColumns = 'name, time, price, participant_limit,description';
+Booking.register();
